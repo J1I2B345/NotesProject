a4a3b0c8983636beda50eb518f942bff2c1c2e63
J1I2B345 <juan.bevilacqua@outlook.com>
2022-07-30 22:45:08 -0300

    src/sockets.js added some try/catch to manage erros

--- src/sockets.js
+++ src/sockets.js
@@ -5,36 +5,55 @@ export default (io) => {
     io.on('connection', (socket) => {
         
         const emitNotes = async() =>{
-        const notes = await Note.find()
-        io.emit('server:loadnotes', notes)
+            try{
+                const notes = await Note.find()
+                io.emit('server:loadnotes', notes)
+            } catch(e){
+                console.log(e.message)    
+            }
         }
         emitNotes()
 
         //para guardar notas en mongoDB
         socket.on('client:newnote', async (data) =>{
         
+            try{
             const newNote = new Note(data)
             const savedNote = await newNote.save()
-            
             io.emit('server:newnote', savedNote)
+            } catch(e){
+                console.log(e.message)
+            }
         })
 
         socket.on('client:deletenote', async(id)=>{
-            await Note.findByIdAndDelete(id)
-            emitNotes()
+            try{
+                await Note.findByIdAndDelete(id)
+                emitNotes()
+            } catch(e){
+                console.log(e.message)
+            }
         })
 
         socket.on('client:getnote', async(id)=>{
-            const noteToUpdate = await Note.findById(id)
-            socket.emit('server:getnote', noteToUpdate)
+            try{
+                const noteToUpdate = await Note.findById(id)
+                socket.emit('server:getnote', noteToUpdate)
+            } catch(e){
+                console.log(e.message)
+            }
         })
 
         socket.on('client:updatenote', async(note)=>{
-            await Note.findByIdAndUpdate(note._id, {
-                title: note.title,
-                description: note.description
-            })    
-            emitNotes()         
+            try{ 
+                await Note.findByIdAndUpdate(note._id, {
+                    title: note.title,
+                    description: note.description
+                })    
+                emitNotes()}
+            catch(e){
+                console.log(e.message)
+            }         
         })
 
     })
